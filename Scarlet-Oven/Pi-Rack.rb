59de4c1e94d7e9be5afe77332ade8f015e0a5ec9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-30 17:51:01 -0700

    Pi-Rack.rb [V1]
    Version 1 of the Pi Rack file fort his proejct.

---
+++ Scarlet-Oven/Pi-Rack.rb
@@ -0,0 +1,144 @@
+#!/usr/bin/env ruby
+# Start of script
+# The Pi(e) rack, where the ovens contents are stored
+class pi-rack
+	def ovenFile
+		@ovenFile = "/Oven/OvenFile.yml"
+		break
+	def pi_rack
+		def a3DPrintedPie # The a is added, as Ruby does not support function names starting with a numeral
+			@a3DPrintedPie = "/Oven/3D-Printed-Pie/"
+			break
+		def androPie
+			@androPie = "/Oven/AndroPie/"
+			break
+		def applePie
+			@applePie = "/Oven/Apple-Pie/"
+			break
+		def baconPie
+			@baconPie = "/Oven/Bacon-Pie/"
+			break
+		def bananaPie
+			@bananaPie = "/Oven/Banana-Pie/"
+			break
+		def burntPie
+			@burntPie = "/Oven/Burnt-Pie/"
+			break
+		def blueberryPie
+			@blueberryPie = "/Oven/Blueberry-Pie/"
+			break
+		def bananaCreamPie
+			@bananaCreamPie = "/Oven/Banana-Cream-Pie/"
+			break
+		def blackberryPie
+			@blackberryPie = "/Oven/Blackberry-Pie/"
+			break
+		def cherryPie
+			@cherryPie = "/Oven/Cherry-Pie/"
+			break
+		def creamPie
+			@creamPie = "/Oven/Cream-Pie/"
+			break
+		def chickenPotPie
+			@chickenPotPie = "/Oven/Chicken-Pot-Pie/"
+			break
+		def chocolatePie
+			@chocolatePie = "/Oven/Chocolate-Pie/"
+			break
+		def eelPie
+			@eelPie = "/Oven/Eel-Pie/"
+			break
+		def honeyPie
+			@honeyPie = "/Oven/Honey-Pie/"
+			break
+		def iPie
+			@iPie = "/Oven/iPie/"
+			break
+		def keyLimePie
+			@keyLimePie = "/Oven/Key-Lime-Pie/"
+			break
+		def kiwiPie
+			@kiwiPie = "/Oven/Kiwi-Pie/"
+			break
+		def magPie
+			@magPie = "/Oven/Mag-Pie/"
+			break
+		def peachPie
+			@peachPie = "/Oven/Peach-Pie/"
+			break
+		def pecanPie
+			@pecanPie = "/Oven/Pecan-Pie/"
+			break
+		def pumpkinPie
+			@pumpkinPie = "/Oven/Pumpkin-Pie/"
+			break
+		def pizzaPie
+			@pizzaPie = "/Oven/Pizza-Pie/"
+			break
+		def raspberryPie
+			@raspberryPie = "/Oven/Raspberry-Pie/"
+			break
+		def rhubarbPie
+			@rhubarbPie = "/Oven/Rhubarb-Pie/"
+			break
+		def snakePie
+			@snakePie = "/Oven/Snake-Pie/"
+			break
+		def strawberryPie
+			@strawberryPie = "/Oven/Strawberry-Pie/"
+			break
+		def vanillaPie
+			@vanillaPie = "/Oven/Vanilla-Pie/"
+			break
+		def virtualPie
+			@virtualPie = "/Oven/Virtual-Pie/"
+			break
+		return 3DPrintedPie();
+		return androPie();
+		return applePie();
+		return baconPie();
+		return bananaPie();
+		return burntPie();
+		return blueberryPie();
+		return bananaCreamPie();
+		return blackberryPie?();
+		return cherryPie();
+		return creamPie();
+		return chickenPotPie();
+		return chocolatePie();
+		return eelPie();
+		return honeyPie();
+		return iPie();
+		return keyLimePie();
+		return kiwiPie();
+		return magPie();
+		return peachPie();
+		return pecanPie();
+		return pumpkinPie();
+		return pizzaPie();
+		return raspberryPie();
+		return rhubarbPie();
+		return snakePie();
+		return strawberryPie();
+		return vanillaPie();
+		return virtualPie();
+		break
+	def main
+		return ovenFile();
+		return pi_rack();
+		break;
+	return main();
+	break
+end
+"""
+Todo
+Add support for different typing styles (not just snake-case)
+Add new flavors of pie
+Define Pie flavors better
+Make the program functional
+"""
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (2022, Saturday, April 30th at 5:51 pm PST)
+# Line count (including blank lines and compiler line): 145
+# End of script
